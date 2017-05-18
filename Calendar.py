6698d37026fd1a1d1625cd635d790f5929f31d94
OCHIENG EVANS <ochiengevans4@gmail.com>
2017-05-18 17:42:30 +0300

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -1,5 +1,9 @@
+#NAME:OCHIENG EVANS
+#REGISTRATION NO.:16/K/2260/PS
+#STUDENT NO.:216002413
+#COMPUTER YEAR 1
 import calendar #this is an inbuilt module that will enable us get the week day
-name = input("Enter your name:")#User is to input name
+name = input("Enter your name")#ser to input name
 print('Hello',name,',welcome to our program')
 date = int(input('Enter your date of birth:'))
 month = int(input('Enter your month of birth:'))
@@ -7,4 +11,4 @@ year = int(input('Enter your year of birth:'))
 get = calendar.weekday(year,month,date)#gets the corresponding day of the week
 selection = {0:'Monday',1:'Tuesday',2:'Wednesday',3:'Thursday',4:'Friday',5:'Saturday',6:'Sunday'}
 '''This is a dictionary with the key as a number and value as a weekday'''
-print(name,',you were born on a',selection[get])
+print(name,',you were born on a',selection[get])#prints user name and birthday
